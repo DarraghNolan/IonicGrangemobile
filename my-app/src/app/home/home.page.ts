8d13d729b0ecb4f3e3dae5488958088fd130d9af
DarraghNolan <c17366881@mytudublin.ie>
2023-04-30 17:33:21 +0100

    attempted to get firebase working

--- my-app/src/app/home/home.page.ts
+++ my-app/src/app/home/home.page.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
+import { FirebaseService } from '../services/firebase.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
 
-  constructor() {}
+export class HomePage {
+  Announcements: any = [];
+
+  constructor(private firebaseService: FirebaseService) {
+
+  this.firebaseService.getEvents().subscribe((data)=>{
+    this.Announcements=data;
+  })
+  }
 
 }
--- my-app/src/app/services/firebase.service.ts
+++ my-app/src/app/services/firebase.service.ts
@@ -1,9 +1,24 @@
 import { Injectable } from '@angular/core';
+import { Firestore, collection, collectionData } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
+
+export interface Announcement {
+  id?: string;
+  Announcement1:string;
+  Announcement2:string;
+} 
 
 @Injectable({
   providedIn: 'root'
 })
 export class FirebaseService {
 
-  constructor() { }
-}
+  constructor(private firestore: Firestore) { 
+  }
+
+    getEvents():Observable<Announcement[]> {
+      const eventRef= collection(this.firestore,'Announcement')
+      return collectionData(eventRef,{idField:'id'}) as Observable<Announcement[]>
+    }
+
+  }
